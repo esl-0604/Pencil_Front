2267f0b45b4716d008432741f3033eb36f662e62
jhb0474 <jhb0474@naver.com>
2022-11-18 16:13:42 +0900

    mapScreen

--- App.js
+++ App.js
@@ -6,20 +6,7 @@ import SigninScreen from "./Screen/SigninScreen";
 import MapScreen from "./Screen/MapScreen";
 
 export default function App() {
-  const [latitude, setLatitude] = useState(null);
-  const [longitude, setLongitude] = useState(null);
-  const [ok, setOk] = useState(true);
-  const ask = async() => {
-    const {granted} = await Location.requestForegroundPermissionsAsync();
-    if (!granted){
-      setOk(false);
-    };
-    const location = await Location.getCurrentPositionAsync({accuracy:6});
-    setLatitude(location.coords.latitude);
-    setLongitude(location.coords.longitude);
-    console.log("Latitude : " + latitude);
-    console.log("Longitude : " + longitude);
-  };
+
   
   const [view, setView] = useState("3");
   if (view == "1"){
